6e11310ac99f515ca006f09d517d3cacbfa1e3bb
Shaggy <nesandub7@gmail.com>
2026-08-19 01:59:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/nesandub7-spec/SHAGGY-OFC/blob/main/images/WhatsApp%20Image%202026-08-19%20at%201.58.05%20AM.jpeg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 SHAGGY-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
